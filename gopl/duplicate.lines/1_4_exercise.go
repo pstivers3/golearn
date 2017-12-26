efbd8af5aa21a6fce639ba59c8ec6050e536ba01
Paul Stivers <paul.stivers@fidelitylife.com>
2017-12-26 00:09:48 -0800

    first commit

---
+++ gopl/duplicate.lines/1_4_exercise.go
@@ -0,0 +1,45 @@
+// prints the count and text of lines that appear more than once
+// in the input. It reads from stdin or from a list of files.
+
+package main
+
+import(
+    "bufio"
+    "fmt"
+    "os"
+)
+
+func main() {
+    counts := make(map[string]int)
+    files := os.Args[1:]
+    if len(files) == 0 {
+        countLines(os.Stdin, counts)
+    } else {
+        for _, arg := range files {
+            f, err := os.Open(arg)
+            if err != nil {
+                fmt.Fprintf(os.Stderr, "2up: %v\n", err)
+                continue // goes to the next iteration of the for loop
+            }
+            countLines(f, counts)
+            f.Close()
+        }
+    }
+    for line, n := range counts {
+        if n > 1 {
+            fmt.Printf("%d\t%s\n", n, line)
+        }
+    }
+}
+
+func countLines(f *os.File, counts map[string]int) {
+    input := bufio.NewScanner(f)
+    for input.Scan() {
+        counts[input.Text()]++
+        if counts[input.Text()] > 0 {
+            fmt.Println(f)
+        }
+    }
+    // Note: ignore potential errors from input.Err()
+}
+
