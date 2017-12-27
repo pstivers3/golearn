3af8d31d3c2da8385e05ee8ad018cbf902f0b746
Paul Stivers <paul.stivers@fidelitylife.com>
2017-12-27 15:04:40 -0800

    first commit

---
+++ gopl/fetch_url/fetch_url.go
@@ -0,0 +1,30 @@
+// prints the content found at a URL
+// usage:
+// $ go build filename.go
+// $ ./filename <url>, for example http://gopl.io
+
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		resp, err :=http.Get(url)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetc: %v\n", err)
+			os.Exit(1)
+		}
+		b, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+			os.Exit(1)
+		}
+		fmt.Printf("%s", b)
+	}
+}
+
