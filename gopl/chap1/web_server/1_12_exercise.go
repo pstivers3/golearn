aa20c66e8f66605cfd432b9a2fdbe2759037b7e1
Paul Stivers <paul.stivers@fidelitylife.com>
2018-01-08 23:04:11 -0800

    first commit

---
+++ gopl/chap1/web_server/1_12_exercise.go
@@ -0,0 +1,77 @@
+// serve lissajous figures
+// doesn't work!!
+
+package main
+
+import (
+	"image"
+    "image/color"
+    "image/gif"
+    "io"
+    "math"
+    "math/rand"
+	"os"
+)
+
+//packages not needed by example in book
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"time"
+)
+
+
+const (
+    whiteIndex = 0 // first color in palette
+    blackIndex = 1 // next color in palette
+)
+
+func main () {
+	var palette = []color.Color{color.White, color.Black} //compostite literal slice
+	rand.Seed(time.Now().UTC().UnixNano())
+
+	if len(os.Args) > 1&& os.Args[1] == "web" {
+		handler := func(w http.ResponseWriter, r *http.Request) {
+			cyclesStr := func(w http.ResponseWriter, r *http.Request) {
+			if cyclesStr != "" {
+				var err error
+				cycles, err = strconv.Atoi(cyclesStr)
+				if err != nil {
+					fmt.Fprintf(w, "bad cycles param: %s", cyclesStr, err)
+					return
+				}
+			}
+			lissajous(cycles, w)
+		}
+		http.HandleFunc("/", handler)
+		log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func lissajous(out io.Writer) {
+	const (
+        res     = 0.001 // angular resolution
+        size    = 100   // image canvas covers [-size..+size]
+        nframes = 64    // number of animation frames
+        delay   = 8     // delay between frames in 10ms units
+    )
+    freq := rand.Float64() * 3.0 // relative frequency and y oscillator
+    anim := gif.GIF{LoopCount: nframes} // composite literal struct
+    phase := 0.0 // phase difference
+    for i := 0; i < nframes; i++ {   // each iteration produces a single frame for the gif
+        rect := image.Rect(0, 0, 2*size+1, 2*size+1)
+        img := image.NewPaletted(rect, palette)
+        for t := 0.0; t< cycles*2*math.Pi; t += res {
+            x := math.Sin(t)
+            y := math.Sin(t*freq + phase)
+            img.SetColorIndex(size +int(x*size+0.5), size+int(y*size+0.5),
+                blackIndex)
+        }
+        phase += 0.1
+        anim.Delay = append(anim.Delay, delay)
+        anim.Image = append(anim.Image, img)
+    }
+	gif.EncodeAll(out, &anim) // Note: ignoring encoding errors
+}
+
