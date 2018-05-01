cdf6e30ee2826fb4af18e27417033450a77e6eb3
Paul Stivers <paul.stivers@fidelitylife.com>
2018-05-01 16:58:22 -0700

    add more functionality, pun intended

--- gopl/experiments/pointers/new.go
+++ gopl/experiments/pointers/new.go
@@ -7,6 +7,7 @@ import (
 func main () {
     fmt.Println(newInt1())
     fmt.Println(newInt2())
+    fmt.Println(delta(2,5))
 }
 
 func newInt1() *int {
@@ -16,6 +17,10 @@ func newInt1() *int {
 func newInt2() *int {
 	var dummy int
 	return  &dummy
+}
+
+func delta(old, new int) int { // 'new' as an argument in a function is now just a variable name
+	return new - old
 }
 
 
