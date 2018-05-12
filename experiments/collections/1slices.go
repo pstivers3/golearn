2c51926c99b55c1405035aad569653407509c273
Paul Stivers <pstivers3@comcast.net>
2018-05-12 02:02:15 -0700

    first commit

--- experiments/collections/1slices.go
+++ experiments/collections/1slices.go
@@ -19,8 +19,7 @@ func main() {
     }
 
 	s := []string{"a","b","c"}
-
-	// make a copy of s into c
+	// make c and then copy of s into c
 	c := make([]string, len(s))
 	copy(c, s)
 	c = append(c, "d","e")
---
+++ experiments/collections/2slices.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	a := [5]int{0,1,2,3,4}
+	sat := a[0:5]
+	fmt.Println(sat)
+	sata := append(sat, 5, 6)
+	fmt.Println(sata)
+	sat[0] = 22
+	fmt.Println(sat)
+}
