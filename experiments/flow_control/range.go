52c375297e6e1f0caed199caa7052d265b9a5365
Paul Stivers <pstivers3@comcast.net>
2018-05-13 19:42:30 -0700

    first commit

---
+++ experiments/flow_control/range.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{6,7,8}
+	for index, value := range s { // returns the index and value if both receiving variables are specified
+		fmt.Println(index, value)
+	}
+	for index := range s { // returns the index if only one receiving variable is specified
+		fmt.Println(index)
+	}
+	for _, value := range s { // if only the value is needed, use the emty identifier for the index
+		fmt.Println(value)
+	}
+}
