e8e389104a18be825a96f95ecd083e5f7f47e505
Paul Stivers <pstivers3@comcast.net>
2018-05-25 23:30:34 -0700

    add comment

--- experiments/goroutines/1goroutine.go
+++ experiments/goroutines/1goroutine.go
@@ -5,20 +5,17 @@ package main
 import (
 	"fmt"
 	"time"
-	"math/rand"
 )
 
 func say(s string) {
 	for i := 0; i < 3; i++ {
-		// //  sleep random milliseconds up to 250
-		// amt := time.Duration(rand.Intn(250))
-		// time.Sleep(time.Millisecond * amt)
 		time.Sleep(100 * time.Millisecond) // sleep for 100 milliseconds
 		fmt.Println(s)
 	}
 }
 
 func main() {
-	go say("world") // if the second say() is a goroutine or if both are grs, the grs don't print ?? q
-	say("hello")
+	// the non-gr call needs to be second, to give the gr call time to run 
+	go say("one")
+	say("two")
 }
