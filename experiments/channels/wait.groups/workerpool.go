6c5b54c7a8be02f9d5bff5d50248aad25348e08d
Paul Stivers <paul.stivers@fidelitylife.com>
2018-06-07 18:18:49 -0700

    works

--- experiments/channels/wait.groups/workerpool.go
+++ experiments/channels/wait.groups/workerpool.go
@@ -24,7 +24,7 @@ func digits(number int) int {
 	sum := 0
 	no := number
 	for no != 0 {
-		digit = no % 10
+		digit := no % 10
 		sum += digit
 		no /= 10
 	}
@@ -34,15 +34,15 @@ func digits(number int) int {
 
 func worker(wg *sync.WaitGroup) {
 	for job := range jobs {
-		output := Results {job, digits(job.randomno)}
+		output := Result{job, digits(job.randomno)}
 		results <- output
 	}
-	wg.Done
+	wg.Done()
 }
 
-func createWorkerPool(noOfWorkers in) {
+func createWorkerPool(noOfWorkers int) {
 	var wg sync.WaitGroup
-	for i := 0 i < noOfWorkers; i++ {
+	for i := 0; i < noOfWorkers; i++ {
 		wg.Add(1)
 		go worker(&wg)
 	}
@@ -54,8 +54,30 @@ func allocate(noOfJobs int) {
 	for i := 0; i < noOfJobs; i++ {
 		randomno := rand.Intn(999)
 		job := Job{i, randomno}
+		jobs <- job
 	}
 	close(jobs)
 }
 
+func result(done chan bool) {
+	for result := range results {
+		fmt.Printf("Job id %d, input random no %d, sum of digits %d\n",
+		    result.job.id, result.job.randomno, result.sumofdigits)
+	}
+	done <- true
+}
+
+func main() {
+	startTime := time.Now()
+	noOfJobs := 100
+	go allocate(noOfJobs)
+	done := make(chan bool)
+	go result(done)
+	noOfWorkers := 10
+	createWorkerPool(noOfWorkers)
+	<-done
+	endTime := time.Now()
+	diff := endTime.Sub(startTime)
+	fmt.Println("total time taken ", diff.Seconds(), "seconds")
+}
 
