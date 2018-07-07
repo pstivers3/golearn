9e96650579a212c607a00917a9dea5088b9e7ef3
Paul Stivers <pstivers3@comcast.net>
2018-07-07 09:32:30 -0700

    m

---
+++ experiments/functions/fizz_buzz2.go
@@ -0,0 +1,49 @@
+// print the numbers from 1 to 100, exept:
+// if multiple of 3 print Fizz
+// if multiple of 5 print Buzz
+// if multiple of 3 and 5 print FizzBuzz
+
+package main
+
+import (
+	"fmt"
+)
+
+func isFactor3 (num int) bool {
+	if num%3 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isFactor5 (num int) bool {
+	if num%5 == 0 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func isFactorBoth (test1 bool, test2 bool) bool {
+	if test1 && test2 {
+		return true
+	} else {
+		return false
+	}
+}
+
+func main() {
+	for i := 1; i <= 100; i++ {
+		if isFactorBoth (isFactor3(i), isFactor5(i)) {
+			fmt.Println("FizzBuzz")
+		} else if isFactor3(i) {
+			fmt.Println("Fizz")
+		} else if isFactor5(i) {
+			fmt.Println("Buzz")
+		} else {
+			fmt.Println(i)
+		}
+	}
+}
+
