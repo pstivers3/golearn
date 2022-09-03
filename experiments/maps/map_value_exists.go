e5f929b2bc8150dc59b3402c583d1557729b3907
Paul Stivers <paul.stivers@fidelitylife.com>
2022-09-03 00:17:56 -0400

    changes

--- experiments/maps/map_value_exists.go
+++ experiments/maps/map_value_exists.go
@@ -5,13 +5,14 @@ import "fmt"
 func main() {
 	m := map[string]int{"n1": 1, "n2": 2}
 
-	// boolean of if ["n1"] exists is stored in ok
-	if value, ok := m["n1"]; ok {
+	// boolean of if ["n1"] exists is stored in exists
+	if value, exists := m["n1"]; exists {
 		fmt.Println("value of \"n1\" is ", value)
 	} else {
 		fmt.Println("value of \"n1\" has unknown value")
 	}
 
+	// boolean of if ["n3"] exists is stored in ok
 	if value, ok := m["n3"]; ok {
 		fmt.Println("value of \"n3\" is ", value)
 	} else {
--- experiments/maps/test_mulitple_map_properties.go
+++ experiments/maps/test_mulitple_map_properties.go
@@ -5,9 +5,9 @@ import (
 )
 
 func main() {
-	m := map[string]int{"k1": 7, "k2": 14}
-	m["k3"] = 21
-	delete(m, "k2")
+	m := map[string]int{"k1": 2, "k2": 4, "k3": 6}
+	m["k4"] = 8
+	delete(m, "k1")
 	fmt.Println(m)
 	v, prsnt := m["k1"]
 	fmt.Println(v, prsnt, "\n")
@@ -17,13 +17,15 @@ func main() {
 	_, prsnt = m["k2"]
 	fmt.Println(prsnt)
 	v = m["k3"]
-	fmt.Println(v, m["k3"], "\n")
+	fmt.Println(v, m["k3"])
+	v, _ = m["k4"]
+	fmt.Println(v, m["k4"], "\n")
 
 	for k, v := range m {
 		fmt.Printf("%s -> %d\n", k, v)
 	}
 
 	for k := range m {
-        fmt.Println("key:", k)
+		fmt.Println("key:", k)
 	}
 }
