e9c947ee46900e2341527765f086a246537e6dc9
Paul Stivers <paul.stivers@fidelitylife.com>
2022-09-09 19:28:49 -0400

    add comments

--- experiments/testing/main_test.go
+++ experiments/testing/main_test.go
@@ -1,9 +1,15 @@
+// https://www.youtube.com/watch?v=Kf5LLi7Ti9A&t=12s
+
+// To run, go test
+// Assert should fail with passord of at least 5 characters.
+// However it passes.
+
 import(
 	"testing"
 	"github.com/stretchr/testify/assert"
 )
 
-// assert should fail, however it passes
+
 
 func TestPassword_Validate(t *testing.T) {
 	p := Password("321123")
